1d980a083a074e6d1cb67db8f10dd44d953f7d12
leadgfl13 <leadgfl13@gmail.com>
2023-12-28 17:22:04 -0500

    Add function for keys a through h for sounds later

--- script.js
+++ script.js
@@ -3,7 +3,35 @@ let keyA = document.getElementById('keyA')
 addEventListener('keypress', keyEvent)
 
 function keyEvent(e){
-    if(e.keyCode = 'a'){
-        alert("hjello")
+    let key = e.key
+
+ if (key =='b' || key == "B"){
+        alert("hello b")
     }
+if (key == 'a' || key == 'A'){
+    alert('hello a')
+}
+
+if (key =='c' || key == "C"){
+    alert("hello c")
+}
+if (key == 'd' || key == 'D'){
+alert('hello d')
+}
+
+if (key =='e' || key == "E"){
+    alert("hello e")
+}
+if (key == 'f' || key == 'F'){
+alert('hello f')
+}
+if (key =='g' || key == "G"){
+    alert("hello g")
+}
+if (key == 'h' || key == 'H'){
+alert('hello h')
+}
+
+
+
 }
