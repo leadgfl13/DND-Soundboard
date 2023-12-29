4591db29880d8d0a1f77d19341fd05377da21b83
leadgfl13 <leadgfl13@gmail.com>
2023-12-29 15:45:00 -0500

    Add a random function that takes the array and radomizes a number.  Add function to a Key to randomly play an audio file from the door array

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-let a = new Audio('   ')
+
+
+// This section contains the door audio files and a variable that is a random door audio pulled from the array//
+let door1 = new Audio('')
+let door2 = new Audio('')
+let door3= new Audio('')
+let door4 = new Audio('')
+
+let doors = [door1, door2, door3, door4]
+let randomdoor = Math.floor(Math.random()* doors.length)
+
+
+//takes an array, and then returns a random number based on the specific array to the global scope via return function)
+createRandom(arrayname){
+let random = Math.floor(Math.random()*arrayname.length)
+return(random)
+}
+
+
 let b = new Audio('')
 let c = new Audio (' ')
 let d = new Audio(' ')
 let e = new Audio('')
 let f = new Audio(' ')
 let g = new Audio(' ')
-let h = new Audio(' )')
+let h = new Audio(' ')
+
+
 addEventListener('keypress', keyEvent)
 
 function keyEvent(e){
     let key = e.key
 
+ if (key == 'a' || key == 'A'){
+        alert('hello a')
+        createRandom(doors)
+        doors.random.play()
+
+    }    
+
  if (key =='b' || key == "B"){
         a.play()
     }
-if (key == 'a' || key == 'A'){
-    alert('hello a')
-}
 
 if (key =='c' || key == "C"){
     alert("hello c")
